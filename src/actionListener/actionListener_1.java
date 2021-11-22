c0d1fe65562dae7c18f29d913fc485a6e01deae4
IvánEstebanBenedicto <IvnEstebanBenedicto@2DAM-09>
2021-11-22 11:46:31 +0100

    primer commit

---
+++ src/actionListener/actionListener_1.java
@@ -0,0 +1,16 @@
+
+package actionListener;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class actionListener_1 implements ActionListener{
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+        
+        
+    }
+    
+}
